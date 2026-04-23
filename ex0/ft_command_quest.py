274d8fe9a4cc1381baff399c6867f1e07186343b
ksener <blogkeremsener@gmail.com>
2026-04-23 10:32:08 +0300

    Add initial implementation of command-line argument processing

---
+++ ex0/ft_command_quest.py
@@ -0,0 +1,15 @@
+import sys
+
+if __name__ == "__main__":
+    print("=== Command Quest ===")
+    total_arg = len(sys.argv)
+    print(f"Program name: {sys.argv[0]}")
+    if total_arg <= 1:
+        print("No arguments provided!")
+    else:
+        print(f"Arguments received: {total_arg - 1}")
+        i = 1
+        while i < total_arg:
+            print(f"Argument {i}: {sys.argv[i]}")
+            i += 1
+    print(f"Total arguments: {total_arg}")
