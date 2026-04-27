e4c10c4baa0f38bb5fd2d27f33f92d4bd0f37782
ksener <blogkeremsener@gmail.com>
2026-04-27 12:49:55 +0300

    Refactor get_player_pos function for improved input handling and clarity

--- ex2/ft_coordinate_system.py
+++ ex2/ft_coordinate_system.py
@@ -1,19 +1,39 @@
-def get_player_pos(numb: list) -> tuple:
+#!/usr/bin/env python3
+import math
 
-    return (float(numb[0]), float(numb[1]), float(numb[2]))
 
-
-def convert_float():
+def get_player_pos() -> tuple:
+    coordinate = (0, 0, 0)
     while True:
         try:
-            split = input("Enter new coordinates as floats in format 'x,y,z':\
- ").split(",")
-            split = get_player_pos(split)
-            break
+            text = input("Enter new coordinates as floats in format 'x,y,z': ")
+            try:
+                coordinate = text.split(",")
+            except ValueError as e:
+                print(e)
+            return (float(coordinate[0]), float(coordinate[1]),
+                    float(coordinate[2]))
         except ValueError:
             print("Invalid syntax")
 
 
 if __name__ == "__main__":
-    print("=== Game Coordinate System ===")
-    convert_float()
+    print("=== Game Coordinate System ===\n")
+    print("Get a first set of coordinates")
+    array = []
+    player_pos = get_player_pos()
+    array.append(player_pos)
+    print(f"Got a first tuple: {player_pos}")
+    text = "It includes:"
+    print(f"{text} X={player_pos[0]}, Y={player_pos[1]}, Z={player_pos[2]}")
+    distance = math.sqrt((player_pos[0] - 0)**2 + (player_pos[1] - 0)**2 +
+                         (player_pos[2] - 0)**2)
+
+    print("Distance to center:", round(distance, 4))
+    print("Get a second of coordinates")
+    second_pos = get_player_pos()
+    second_distance = math.sqrt((player_pos[0] - second_pos[0])**2 +
+                                (player_pos[1] - second_pos[1])**2 +
+                                (player_pos[2] - second_pos[2])**2)
+    text = "Distance between the 2 sets of coordinates:"
+    print(f"{text} {round(second_distance, 4)}")
