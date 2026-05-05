9fac8b821fcc61b0a276c9feecfbf0a86392b3ad
ksener <blogkeremsener@gmail.com>
2026-05-05 12:39:29 +0300

    All range() func removed and changed to while loop.
    Also public "achievements_pool" moved to gen_player_achievements() func.

--- ex3/ft_achievement_tracker.py
+++ ex3/ft_achievement_tracker.py
@@ -1,19 +1,18 @@
 #!/usr/bin/env python3
 import random
 
-achievements_pool = [
-    "Crafting Genius", "Strategist", "World Savior",
-    "Speed Runner", "Survivor", "Master Explorer",
-    "Treasure Hunter", "Unstoppable", "First Steps",
-    "Collector Supreme", "Untouchable", "Sharp Mind",
-    "Boss Slayer", "Hidden Path Finder"
-]
-
 
 def gen_player_achievements() -> set[str]:
+    achievements_pool = [
+        "Crafting Genius", "Strategist", "World Savior",
+        "Speed Runner", "Survivor", "Master Explorer",
+        "Treasure Hunter", "Unstoppable", "First Steps",
+        "Collector Supreme", "Untouchable", "Sharp Mind",
+        "Boss Slayer", "Hidden Path Finder"
+    ]
     random_numb = random.randint(4, 8)
     player_achievement = set(random.sample(achievements_pool, random_numb))
-    return (player_achievement)
+    return player_achievement
 
 
 class Control:
@@ -32,33 +31,43 @@ class Control:
         return set.union(*self.array)
 
     def exclusive_control(self) -> None:
-        for i in range(len(self.array)):
+        i = 0
+        total_len = len(self.array)
+        while i < total_len:
             current_player_set = self.array[i]
             current_player_name = self.name[i]
-
             exclusive_set = current_player_set.copy()
 
-            for j in range(len(self.array)):
+            j = 0
+            while j < total_len:
                 if i != j:
                     exclusive_set = exclusive_set.difference(self.array[j])
+                j += 1
             print(f"Only {current_player_name} has: {exclusive_set}")
+            i += 1
 
     def missing_control(self) -> None:
         total = self.all_distinct_control()
-        for i in range(len(self.array)):
+        i = 0
+        total_len = len(self.array)
+        while i < total_len:
             current_player_set = self.array[i]
             current_player_name = self.name[i]
             missing_set = total.difference(current_player_set)
             print(f"{current_player_name} is missing: {missing_set}")
+            i += 1
 
 
 def achievement_tracker() -> None:
-    i = 0
     player_name = ["Alice", "Bob", "Charlie", "Dylan"]
-    player_achievements: list[set[str]] = [set() for _ in range(4)]
-    for i in range(4):
-        player_achievements[i] = gen_player_achievements()
-        print(f"Player {player_name[i]}: {player_achievements[i]}")
+    player_achievements: list[set[str]] = []
+
+    i = 0
+    while i < len(player_name):
+        new_set = gen_player_achievements()
+        player_achievements.append(new_set)
+        print(f"Player {player_name[i]}: {new_set}")
+        i += 1
 
     my_control = Control(player_achievements, player_name)
 
